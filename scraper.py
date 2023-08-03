58d7e7b14a5d073738afbd4927765a7ee5513215
Luke Tao <automaticelk@gmail.com>
2023-08-03 13:24:07 -0400

    Add ability to scrape multiple words

--- scraper.py
+++ scraper.py
@@ -1,10 +1,13 @@
+import csv
 import requests_html
+from bs4 import BeautifulSoup
 
 ses = requests_html.HTMLSession()
 
 
-def scrapeWord(hanzi, numDefs, numExamples):
+def scrapeChar(hanzi, numDefs, numExamples):
     info = dict()
+    info["hanzi"] = hanzi
 
     r = ses.get(
         "https://www.archchinese.com/chinese_english_dictionary.html?find=" + hanzi
@@ -19,19 +22,26 @@ def scrapeWord(hanzi, numDefs, numExamples):
         .split("\n")
     )
 
+    # Get all information on page
     details = dict()
-
     for detail in charDef:
         x = detail.split(":")
         if len(x) >= 2:
             details[x[0].strip()] = x[1].strip()
 
+    # Extract information we want
     info["definition"] = ", ".join(details["Definition"].split(", ")[:numDefs])
     pinyinLst = details["Pinyin"].split(", ")
     info["pinyin"] = pinyinLst[0]
-    info["pinyin2"] = ", ".join(pinyinLst[1:])
-    info["hsk"] = details["HSK Level"]
-    info["formation"] = details["Formation"]
+    info["pinyin2"] = ", ".join(pinyinLst[1:]) if len(pinyinLst) > 1 else ""
+    if "HSK Level" in details:
+        info["hsk"] = details["HSK Level"]
+    else:
+        info["hsk"] = ""
+    if "Formation" in details:
+        info["formation"] = details["Formation"]
+    else:
+        info["formation"] = ""
 
     # Get examples
     wordTable = r.html.find("#wordPaneContent #wordTable", first=True)
@@ -39,7 +49,7 @@ def scrapeWord(hanzi, numDefs, numExamples):
     exInfo = wordTable.find(".col-md-7")
 
     examples = []
-    for i in range(numExamples):
+    for i in range(min(numExamples, len(exWords))):
         hanzi = exWords[i].text.strip()
         pinyin = exInfo[i + 1].find("p>a", first=True).text.strip()
         defn = ", ".join(
@@ -52,8 +62,36 @@ def scrapeWord(hanzi, numDefs, numExamples):
 
         examples.append(hanzi + "[" + pinyin + "]: " + defn)
 
-    info["examples"] = examples
+    info["examples"] = "<br>".join(examples) or ""
     return info
 
 
-print(scrapeWord("你", 3, 3))
+chars = []
+with open("input.txt", encoding="utf8") as f:
+    for line in f:
+        for ch in line:
+            if not ch.isspace():
+                chars.append(ch)
+
+with open("output.csv", "w", newline="", encoding="utf8") as f:
+    writer = csv.writer(f, delimiter="\t")
+    writer.writerow(
+        ["Hanzi", "Definition", "Pinyin", "Pinyin2", "HSK", "Formation", "Examples"]
+    )
+
+    for char in chars:
+        # try:
+        info = scrapeChar(char, 5, 3)
+        writer.writerow(
+            [
+                info["hanzi"],
+                info["definition"],
+                info["pinyin"],
+                info["pinyin2"],
+                info["hsk"],
+                info["formation"],
+                info["examples"],
+            ]
+        )
+    # except Exception as e:
+    #     print("error: " + char + " " + str(e))
